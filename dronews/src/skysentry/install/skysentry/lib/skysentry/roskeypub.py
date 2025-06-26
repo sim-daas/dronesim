1491b0b36f3f53f04158ff60c9a8fdcfda5fc599
ubuntu <aitechmanml@gmail.com>
2025-06-26 15:13:46 +0530

    hello

--- dronews/src/skysentry/install/skysentry/lib/skysentry/roskeypub.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# EASY-INSTALL-DEV-SCRIPT: 'skysentry==0.0.0','roskeypub.py'
-__requires__ = 'skysentry==0.0.0'
-__import__('pkg_resources').require('skysentry==0.0.0')
-__file__ = '/root/dronesim/dronews/src/skysentry/build/skysentry/scripts/roskeypub.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
